c187b03fd439fca052fd72556fc3eda2a53c39b0
stenoritama <shr.nrht.0623@gmail.com>
2017-05-12 02:42:59 +0900

    start

---
+++ src/start/Main.java
@@ -0,0 +1,10 @@
+package start;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new PrintThread("Good!").start();
+        new PrintThread("Nice!").start();
+    }
+
+}
